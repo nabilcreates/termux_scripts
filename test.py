7139c8318db900e411d05790e2d78724cc6b59d4
Nabil Ridhwanshah <nabilrdhwan@gmail.com>
2019-10-18 08:25:18 +0800

    Test of subprocess

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-import os, platform
+import os, platform, subprocess
 
 def main():
     print("OS Name: {} \nPlatform System: {} \nPlatform Release: {}".format(os.name, platform.system(), platform.release()))
+    subprocess.call("pkg i nmap")
 
 if __name__ == "__main__":
     main()
